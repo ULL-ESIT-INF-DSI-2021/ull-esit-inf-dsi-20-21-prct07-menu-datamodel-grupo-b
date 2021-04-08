1fdf457da1f83b728c7558c669f5cfa180a1decf
Manuel Ramón Regalado Peraza <alu0100283433@ull.edu.es>
2021-04-08 16:16:14 +0100

    Prueba getDishType de Dish : Exito.

--- ejercicios/src/claseDish.ts
+++ ejercicios/src/claseDish.ts
@@ -8,3 +8,18 @@ export const enum topoPlato {'entrante', 'primer plato', 'segundo plato',
 export const enum tipoPlato {ENTRANTE, PRIMER_PLATO, SEGUNDO_PLATO, POSTRE};              
 
 //tipoPlato.['primer plato']
+
+export class Dish {
+  private Ingredients: Map<Ingredient, number>;
+
+  constructor(ingredients: [Ingredient, number][], private plato: tipoPlato ) {
+    this.Ingredients = new Map;
+
+    ingredients.forEach((element) => this.Ingredients.set(element[0],
+      element[1]));
+  }
+
+  getDishType(): tipoPlato {
+    return this.plato;
+  }
+}
