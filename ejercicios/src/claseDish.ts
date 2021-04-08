52c40bccdd5344f13b818703305182675fe5fc0c
Manuel Ramón Regalado Peraza <alu0100283433@ull.edu.es>
2021-04-08 17:36:37 +0100

    Prueba getIngredients de Dish : Exito.

--- ejercicios/src/claseDish.ts
+++ ejercicios/src/claseDish.ts
@@ -19,6 +19,27 @@ export class Dish {
       element[1]));
   }
 
+  getIngredients(): [Ingredient, number][] {
+    let allIngredients: [Ingredient, number][] = [];
+
+    // We use the spread operator to convert the Map entries in array elements
+    // and store in an array.
+    const entries = [...this.Ingredients.entries()];
+
+    for(let i = 0; i < entries.length; ++i) {
+      allIngredients.push(entries[i]);
+    }
+
+    // Another way to convert a Map to a tuple array.
+    /*
+    for(let[key, value] of this.Ingredients) {
+      allIngredients.push([key, value]);
+    }
+    */
+
+    return allIngredients;
+  }
+
   getDishType(): tipoPlato {
     return this.plato;
   }
