59b1d5ed37d759b0e56e5c8e90b9921c0a135496
Manuel Ramón Regalado Peraza <alu0100283433@ull.edu.es>
2021-04-09 18:02:49 +0100

    Documentacion de la clase Dish.

--- ejercicios/src/claseDish.ts
+++ ejercicios/src/claseDish.ts
@@ -3,17 +3,30 @@
 import {Ingredient} from './practica-7';
 import {ingredient} from './practica-7';
 
-/*
-export const enum tipoPlato {'entrante', 'primer plato', 'segundo plato',
-                             'postre'};
-*/
-//tipoPlato.['primer plato']
-
+/**
+ * @enum Defines what kind of dish is.
+ */
 export const enum tipoPlato {ENTRANTE, PRIMER_PLATO, SEGUNDO_PLATO, POSTRE};              
 
+/**
+ * A class that conforms a dish made of a series of diverse ingredients and
+ * methods that let configure the dish, adding and/or removing those
+ * ingredients.
+ * @class class Dish
+ */
 export class Dish {
+  /**
+   * @member A Map : Keys are object of class _Ingredient_, values are the
+   * weight of each ingredient (in grams).
+   */
   private Ingredients: Map<Ingredient, number>;
 
+  /**
+   * @param dishName The name of the dish.
+   * @param ingredients An Array (tuple of two elements). Each element presents
+   * and object _Ingredient_ and a number (weight in grams).
+   * @param plato The type of the dish.
+   */
   constructor(private dishName: string, ingredients: [Ingredient, number][],
       private plato: tipoPlato ) {
     this.Ingredients = new Map;
@@ -22,10 +35,19 @@ export class Dish {
       element[1]));
   }
 
-  getDishName() {
+  /**
+   * A getter method which returns the dish name.
+   * @returns A string with the name of the dish.
+   */
+  getDishName(): string {
     return this.dishName;
   }
   
+  /**
+   * A getter. Returns all the ingredients of the dish and each amount of these.
+   * @returns An array (a tuple), in which each element is made of an object
+   * _ingredient_ and a number that represents the ingredient weight in grams.
+   */
   getIngredients(): [Ingredient, number][] {
     let allIngredients: [Ingredient, number][] = [];
 
@@ -47,14 +69,30 @@ export class Dish {
     return allIngredients;
   }
 
+  /**
+   * A getter returning the type of the dish.
+   * @returns A enum _tipoPlato_ with the type of the dish.
+   */
   getDishType(): tipoPlato {
     return this.plato;
   }
 
+  /**
+   * Returns the number of ingredients that the dish is made of.
+   * @returns A _number_ representing the number of ingredients that the dish
+   * consist of.
+   */
   numberOfIngredients(): number {
     return this.Ingredients.size;
   }
 
+  /**
+   * Method that let us add a new ingredient to the dish. Ingredients already
+   * present can not be added another time. The exact same ingredient only can
+   * be added once.
+   * @param ingredient A tuple of two variables: the first one is a _Ingredient_
+   * objetc. The second one is a number, the grams of the ingredient.
+   */
   addIngredient(ingredient: [Ingredient, number]): void {
     // If the same exact ingredient doesn't exists inside the map.
     if(!this.Ingredients.get(ingredient[0])) {
@@ -62,6 +100,11 @@ export class Dish {
     }
   }
 
+  /**
+   * Analyzes the dish aliment groups and calculates which is the more abundant.
+   * @returns An _ingredient_ enum value, the more repeated of alls. Returns the
+   * value -1 if there is no unique predominant group of aliments.
+   */
   predominantGroup(): ingredient {
     let counters = [];
     // With this we get the number of entries in a non-string enumerable in
@@ -110,6 +153,13 @@ export class Dish {
     return result;
   }
 
+  /**
+   * Calculates the total dish nutritional composition. Totals of carbohydrates,
+   * proteins and lipids
+   * @returns An array with the carbohydrates total (1º element), proteins total
+   * (2º element) and lipids total (3º element).
+   * total.
+   */
   nutritionalComposition(): [number, number, number] {
     const entries = [...this.Ingredients.entries()];
 
@@ -127,6 +177,11 @@ export class Dish {
     return [totalCarbohydrates, totalProteins, totalLipids];
   }
 
+  /**
+   * Calculates the dish total price based in the cost of each ingredient used.
+   * @returns The raw total price of the dish : A sum of each ingredient cost
+   * by weight (euros by kilogram used).
+   */
   totalPrice(): number {
     const entries = [...this.Ingredients.entries()];
     let total = 0;
@@ -138,6 +193,12 @@ export class Dish {
     return total;
   }
 
+  /**
+   * Remove all dish ingredients whose name match the argument passed by this
+   * method. 
+   * @param ingredientName Name of the ingredient we want to remove from the
+   * dish.
+   */
   deleteIngredientByName(ingredientName: string): void {
     let ingredientsToRemove: Ingredient[] = [];
     const entries = [...this.Ingredients.entries()];
@@ -155,6 +216,11 @@ export class Dish {
     }
   }
 
+  /**
+   * Removes the ingredient from the dish  
+   * @param ingr An _Ingredient_ object. Deletes the exact match of that object
+   * in the dish if it exists.
+   */
   deleteIngredient(ingr: Ingredient): void {
     this.Ingredients.delete(ingr);
   }
